0cd2f0a8389f8a4d8f749ddc8dbfd8ef5900e957
Jean Mendonça <pierrybm@gmail.com>
2020-09-25 15:43:44 +0000

    Clone finished

--- src/ChatScreen/index.js
+++ src/ChatScreen/index.js
@@ -4,6 +4,7 @@ import './ChatScreen.css';
 import Avatar from '@material-ui/core/Avatar'
 
 const ChatScreen = () => {
+  const [input, setInput] = useState('')
   const [messages, setMessages] = useState([
     {
       name: 'Ellen',
@@ -19,6 +20,13 @@ const ChatScreen = () => {
       message: 'Hi! How are you Ellen!'
     }
   ])
+
+  const handleSend = e => {
+    e.preventDefault()
+
+    setMessages([...messages, { message: input }])
+    setInput('')
+  }
 
   return (
     <div className="chatScreen">
@@ -45,6 +53,18 @@ const ChatScreen = () => {
           )
         ))
       }
+      
+      <form className="chatScreen__input">
+        <input 
+          value={input}
+          onChange={e => setInput(e.target.value)}
+          type="text"
+          className="chatScreen__inputField"
+          placeholder="Type a message..."
+        />
+
+        <button onClick={handleSend} type="submit" className="chatScreen__inputButton">Send</button>
+      </form>
     </div>
   )
 }
